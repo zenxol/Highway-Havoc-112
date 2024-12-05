f77ff0fb423aca757074b675c0e07d8f30926718
zenxol <136659222+zenxol@users.noreply.github.com>
2024-12-05 01:40:37 -0500

    Update coin.py

--- coin.py
+++ coin.py
@@ -1,19 +1,8 @@
 from cmu_graphics import*
 from PIL import Image
-from urllib.request import urlopen
 
-def loadPilImage(url):
-    return Image.open(urlopen(url))
 
 class Coin:
-    image = None
-    # Utilized perplexity.ai to set up preloadImages to reduce lag when spawning obstacle images, in preloadImages function
-    @classmethod
-    def preloadImages(cls):
-        if cls.image == None:
-            coinUrl = 'https://opengameart.org/sites/default/files/styles/medium/public/coin320000.png'
-            pil_image = loadPilImage(coinUrl)
-            cls.image = CMUImage(pil_image)
 
     # Initializes object paraments of coin object
     def __init__(self, app, x, y):
@@ -23,12 +12,12 @@ class Coin:
         self.width = 30
         self.height = 30
         self.speed = 5
-        Coin.preloadImages()
+        self.image = CMUImage(Image.open("images/coin.png"))
 
     # Coin moves similar to obstacles
     def move(self):
         self.y += self.speed
     
     def draw(self):
-        drawImage(Coin.image, self.x, self.y, width=self.width, height=self.height, align='center')
+        drawImage(self.image, self.x, self.y, width=self.width, height=self.height, align='center')
 
