04a386bb0aebb360165a305c48041595e57eb617
zenxol <136659222+zenxol@users.noreply.github.com>
2024-12-05 01:41:36 -0500

    Update warning.py

--- warning.py
+++ warning.py
@@ -1,20 +1,8 @@
 from cmu_graphics import*
 from PIL import Image
-from urllib.request import urlopen
 import time
 
-def loadPilImage(url):
-    return Image.open(urlopen(url))
-
 class Warning:
-    image = None
-    # Utilized perplexity.ai to set up preloadImages to reduce lag when spawning obstacle images, in preloadImages function
-    @classmethod
-    def preloadImages(cls):
-        if cls.image == None:
-            warningUrl = 'https://opengameart.org/sites/default/files/styles/thumbnail/public/warning_0.png'
-            pil_image = loadPilImage(warningUrl)
-            cls.image = CMUImage(pil_image)
 
     def __init__(self, x, y):
         # Initialize warning properties
@@ -24,7 +12,7 @@ class Warning:
         self.flashInterval = 0.1 # Time between flashes in
         self.lastFlash = time.time()
         self.size = 70 
-        Warning.preloadImages()
+        self.image = CMUImage(Image.open("images/warning.png"))
 
     def update(self):
         # Update the visibility of the warning to create a flashing effect 
@@ -36,4 +24,4 @@ class Warning:
     def draw(self):
         # Draw warning image only when visible mode is true
         if self.visible:
-            drawImage(Warning.image, self.x, self.y, width=self.size, height=self.size, align='center')
+            drawImage(self.image, self.x, self.y, width=self.size, height=self.size, align='center')
