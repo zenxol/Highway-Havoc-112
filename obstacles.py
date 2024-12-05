fc1023f802b395219d42601c0c9dc88feffa6ed0
zenxol <136659222+zenxol@users.noreply.github.com>
2024-12-05 01:41:27 -0500

    Update obstacles.py

--- obstacles.py
+++ obstacles.py
@@ -1,30 +1,11 @@
+from cmu_graphics import*
+from PIL import Image
 # https://opengameart.org/sites/default/files/styles/thumbnail/public/YellowBuggy_strip4-4.png.png truck
 # https://opengameart.org/sites/default/files/styles/medium/public/police%20car.png sedan
 # https://www.andrew.cmu.edu/user/gkesden/oldscsstuff/ta/seminarabstracts/images/kosbie.jpg kosbie
-from cmu_graphics import*
-from PIL import Image
-from urllib.request import urlopen
-
-def loadPilImage(url):
-    return Image.open(urlopen(url))
-
 class Obstacle:
-    images = {}
-    # Utilized perplexity.ai to set up preloadImages to reduce lag when spawning obstacle images, in preloadImages function
-    @classmethod
-    def preloadImages(cls):
-        # Dictionary of image URLs for different obstacle types
-        image_urls = {
-            'Truck': 'https://opengameart.org/sites/default/files/styles/thumbnail/public/YellowBuggy_strip4-4.png.png',
-            'Sedan': 'https://opengameart.org/sites/default/files/styles/medium/public/police%20car.png',
-            'Kosbie': 'https://www.andrew.cmu.edu/user/gkesden/oldscsstuff/ta/seminarabstracts/images/kosbie.jpg'
-        }
-        # Load and store images for each obstacle type
-        for name, url, in image_urls.items():
-            pil_image = loadPilImage(url)
-            cls.images[name] = CMUImage(pil_image)
 
-    def __init__(self, app, x, y, width, height, speed, imageName):
+    def __init__(self, app, x, y, width, height, speed):
         # Initialize obstacle properties
         self.app = app
         self.x = x
@@ -32,34 +13,43 @@ class Obstacle:
         self.width = width
         self.height = height
         self.speed = speed
-        self.image = self.images[imageName]
-
+        
     def move(self):
         # Move obstacle downwards
         self.y += self.speed
     
     def draw(self):
-        drawImage(self.image, self.x, self.y, width=self.width, height=self.height, align='center')
+        pass
 
 class Truck(Obstacle):
     def __init__(self, app, x, y):
         # Initialize Truck obstacle with specific dimensions and speed
         width = app.carWidth
         height = app.carHeight
-        super().__init__(app, x, y, width, height, 8, 'Truck')
+        super().__init__(app, x, y, width, height, 8)
+        self.image = CMUImage(Image.open("images/truckAI.png"))
+        
+    def draw(self):
+        drawImage(self.image, self.x, self.y, width=self.width, height=self.height, align='center')
 
 class Sedan(Obstacle):
     def __init__(self, app, x, y):
         # Initialize Sedan obstacle with specific dimensions and speed
         width = app.carWidth
         height = app.carHeight
-        super().__init__(app, x, y, width, height, 10, 'Sedan')
+        super().__init__(app, x, y, width, height, 10)
+        self.image = CMUImage(Image.open("images/sedanAI.png"))
+    def draw(self):
+        drawImage(self.image, self.x, self.y, width=self.width, height=self.height, align='center')
 
 class Kosbie(Obstacle): # Will change to more realistic object later
     def __init__(self, app, x, y):
         # Initialize Kosbie obstacle with specific dimensions and speed
         width = app.carWidth * 0.5
         height = app.carWidth * 0.5
-        super().__init__(app, x, y, width, height, 20, 'Kosbie')
+        super().__init__(app, x, y, width, height, 20)
+        self.image = CMUImage(Image.open("images/kosbieAI.jpg"))
+    def draw(self):
+        drawImage(self.image, self.x, self.y, width=self.width, height=self.height, align='center')
 
 
