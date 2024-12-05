ae5a09750923c8cb5d48ac3475f534383732b8d1
zenxol <136659222+zenxol@users.noreply.github.com>
2024-12-05 01:41:18 -0500

    Update mile.py

--- mile.py
+++ mile.py
@@ -1,20 +1,7 @@
 from cmu_graphics import*
 from PIL import Image
-from urllib.request import urlopen
 
-def loadPilImage(url):
-    return Image.open(urlopen(url))
-
-class Sign:
-    image = None
-    # Utilized perplexity.ai to set up preloadImages to reduce lag when spawning obstacle images, in preloadImages function
-    @classmethod
-    def preloadImages(cls):
-        if cls.image == None:
-            signUrl = 'https://opengameart.org/sites/default/files/styles/medium/public/button_6.png'
-            pil_image = loadPilImage(signUrl)
-            cls.image = CMUImage(pil_image)
-    
+class Sign:  
     # Initializes mile object parameters
     def __init__(self, app, x, y, speed):
         self.app = app
@@ -23,7 +10,7 @@ class Sign:
         self.speed = speed
         self.width = 50
         self.height = 50
-        Sign.preloadImages()
+        self.image = CMUImage(Image.open("images/sign.png"))
     
     # Moves similar to obstacle objects
     def move(self):
