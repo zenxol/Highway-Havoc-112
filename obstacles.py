f73be77448ce211eade6e88c78172ceefbbff063
zenxol <136659222+zenxol@users.noreply.github.com>
2024-12-06 02:57:38 -0500

    Update obstacles.py

--- obstacles.py
+++ obstacles.py
@@ -1,12 +1,9 @@
 from cmu_graphics import*
 from PIL import Image
-# https://opengameart.org/sites/default/files/styles/thumbnail/public/YellowBuggy_strip4-4.png.png truck
-# https://opengameart.org/sites/default/files/styles/medium/public/police%20car.png sedan
-# https://www.andrew.cmu.edu/user/gkesden/oldscsstuff/ta/seminarabstracts/images/kosbie.jpg kosbie
-class Obstacle:
 
+class Obstacle:
+    # Initialize obstacle properties
     def __init__(self, app, x, y, width, height, speed):
-        # Initialize obstacle properties
         self.app = app
         self.x = x
         self.y = y
@@ -28,7 +25,8 @@ class Truck(Obstacle):
         height = app.carHeight
         super().__init__(app, x, y, width, height, 8)
         self.image = CMUImage(Image.open("images/truckAI.png"))
-        
+        # https://opengameart.org/content/yellow-racing-car
+
     def draw(self):
         drawImage(self.image, self.x, self.y, width=self.width, height=self.height, align='center')
 
@@ -39,6 +37,8 @@ class Sedan(Obstacle):
         height = app.carHeight
         super().__init__(app, x, y, width, height, 10)
         self.image = CMUImage(Image.open("images/sedanAI.png"))
+        # https://opengameart.org/content/top-down-pixel-police-car
+
     def draw(self):
         drawImage(self.image, self.x, self.y, width=self.width, height=self.height, align='center')
 
@@ -49,6 +49,8 @@ class Kosbie(Obstacle): # Will change to more realistic object later
         height = app.carWidth * 0.5
         super().__init__(app, x, y, width, height, 20)
         self.image = CMUImage(Image.open("images/kosbieAI.jpg"))
+        # https://www.andrew.cmu.edu/user/gkesden/oldscsstuff/ta/seminarabstracts/images/kosbie.jpg
+
     def draw(self):
         drawImage(self.image, self.x, self.y, width=self.width, height=self.height, align='center')
 
