ff0a00ee5031128d0c5a57ccd0d511c6ac82c3a9
zenxol <136659222+zenxol@users.noreply.github.com>
2024-12-06 02:57:26 -0500

    Update mile.py

--- mile.py
+++ mile.py
@@ -2,7 +2,7 @@ from cmu_graphics import*
 from PIL import Image
 
 class Sign:  
-    # Initializes mile object parameters
+    # Initializes constructor parameters for Sign class
     def __init__(self, app, x, y, speed):
         self.app = app
         self.x = x
@@ -11,10 +11,10 @@ class Sign:
         self.width = 50
         self.height = 50
         self.image = CMUImage(Image.open("images/sign.png"))
-    
+        # https://opengameart.org/content/road-sign-0
     # Moves similar to obstacle objects
     def move(self):
         self.y += self.speed
     
     def draw(self):
-        drawImage(Sign.image, self.x, self.y, width=self.width, height=self.height, align='center')
+        drawImage(self.image, self.x, self.y, width=self.width, height=self.height, align='center')
